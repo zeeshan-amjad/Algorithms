8944e92d4a5807874b55ba2407ea13e91a51b75d
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-09-17 18:50:54 +0530

    Add files via upload

---
+++ Sample-cpp-code/sample.cpp
@@ -0,0 +1,27 @@
+//You're reading zeeshan_amjad's code
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define BOOSTIO ios_base::sync_with_stdio (0), cin.tie (0)
+#define MAX 100005
+#define MOD 1000000007
+#define LL int64_t
+#define LD long double
+#define FOR(i, a, b) for(int i = a; i < b; ++i)
+#define rFOR(i, a, b) for(int i = a; i > b; --i)
+#define PB push_back
+ 
+typedef vector <int> Vector;
+typedef pair <int, int> Pair;
+typedef map <string, int> Map;
+typedef stack <int> Stack;
+typedef queue <int> Queue;
+typedef priority_queue <int> PQ;
+ 
+int main () {
+    BOOSTIO;
+    
+    return 0;
+}
