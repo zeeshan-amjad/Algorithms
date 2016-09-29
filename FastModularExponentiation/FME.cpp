67ac62f8e93f8a3324d478473bdaba36b99bad3a
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-09-29 20:26:15 +0530

    Create FME.cpp

---
+++ FastModularExponentiation/FME.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long fast_pow (long long b, int e) {
+    b = b % MOD;
+    long long ans = 1;
+    while (e > 0) {
+        if (e & 1)
+            ans = (ans * b) % MOD;
+        e /= 2;
+        b = (b * b) % MOD;
+    }
+    return ans % MOD;
+}
+
+int main() {
+    
+	  return 0;
+}
